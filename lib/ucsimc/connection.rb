3152c27bc2eb9dd48a3eae9294f82d281b8eab9f
patrickjr <patrickjr@yosemite.edu>
2015-10-15 10:58:00 -0700

    added logout method

--- lib/ucsimc/connection.rb
+++ lib/ucsimc/connection.rb
@@ -35,23 +35,13 @@ module Ucsimc
       doc = Nokogiri::XML::Document.new
       case @action
       when /login/
-        puts "login"
         @aaa = doc.create_element "aaaLogin", :inName => @user, :inPassword => @pass
       when /refresh/
-        puts "refresh"
         @aaa = doc.create_element "aaaRefresh", :inName => @user, :inPassword => @pass, :inCookie => @cookie
       when /logout/
-        puts "logout"
         @aaa = doc.create_element "aaaLogout", :inCookie => @cookie
       end
        
-    end
-    
-    def create_refresh
-      require 'nokogiri'
-      doc = Nokogiri::XML::Document.new
-      doc = doc.create_element "aaaRefresh", :inName => @user, :inPassword => @pass, :inCookie => @cookie
-      doc
     end
     
     def refresh
@@ -68,7 +58,21 @@ module Ucsimc
       end
     end
     
-    
+    def logout
+      @action = "logout"
+      create_aaa
+      resp = @connection['xmlIM'].post @aaa.to_xml
+      resp_doc = Nokogiri::XML(resp)
+      if resp_doc.root.attribute("outStatus")
+        puts "Disconnection %s" % resp_doc.root.attribute("outStatus").value
+        exit
+      else
+        abort "%s is reporting Error Code:%d %s" % [@host,
+                                                    resp_doc.root.attribute("errorCode").value,
+                                                    resp_doc.root.attribute("errorDescr").value]
+      end
+      
+    end
     
   end
 end

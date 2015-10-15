d5ca5b684cf03cfbd8116abefb11599ec58a686a
patrickjr <patrickjr@yosemite.edu>
2015-10-15 10:20:40 -0700

    changed around how parameters are passed around

--- lib/ucsimc/connection.rb
+++ lib/ucsimc/connection.rb
@@ -4,32 +4,58 @@ module Ucsimc
         
     def initialize user, pass, host
       @user = user
-      pass = pass
+      @pass = pass
       @host = host
-      @aaa = create_aaalogin @user, pass
-      @cookie = get_cookie @aaa
+      create_aaalogin
+      build_connect
+      get_cookie
     end
         
-    def get_cookie aaa
-      require 'rest-client'
-      ucs = RestClient::Resource.new "https://#{@host}", :verify_ssl => false
-      resp = ucs['xmlIM'].post aaa.to_xml
+    def get_cookie
+      #con = build_connect
+      resp = @connection['xmlIM'].post @aaa.to_xml
       resp_doc = Nokogiri::XML resp
       if resp_doc.root.attribute "outCookie"
-        cookie = resp_doc.root.attribute("outCookie").value
+        @cookie = resp_doc.root.attribute("outCookie").value
       else
         abort "There was a problem authenticating. %s is reporting Error Code:%d %s" % [@host,
-                                                                          resp_doc.root.attribute("errorDescr"),
-                                                                          resp_doc.root.attribute("errorCode")]
+                                                                          resp_doc.root.attribute("errorCode").value,
+                                                                          resp_doc.root.attribute("errorDescr").value]
       end
-      cookie
+      #cookie
+    end
+    
+    def build_connect
+      require 'rest-client'
+      @connection = RestClient::Resource.new "https://#{@host}", :verify_ssl => false
     end
         
-    def create_aaalogin user, pass
+    def create_aaalogin
       require 'nokogiri'
       doc = Nokogiri::XML::Document.new
-      doc = doc.create_element "aaaLogin", :inName => user, :inPassword => pass 
+      @aaa = doc.create_element "aaaLogin", :inName => @user, :inPassword => @pass 
+      #@aaa
+    end
+    
+    def create_refresh
+      require 'nokogiri'
+      doc = Nokogiri::XML::Document.new
+      doc = doc.create_element "aaaRefresh", :inName => @user, :inPassword => @pass, :inCookie => @cookie
       doc
+    end
+    
+    def refresh
+      aaarefresh = create_refresh
+#      con = build_connect
+      resp = @connection['xmlIM'].post aaarefresh.to_xml
+      resp_doc = Nokogiri::XML(resp)
+      if resp_doc.root.attribute "outCookie"
+        @cookie = resp_doc.root.attribute("outCookie").value
+      else
+        abort "There was a problem authenticating. %s is reporting Error Code:%d %s" % [@host,
+                                                                                resp_doc.root.attribute("errorCode").value,
+                                                                                resp_doc.root.attribute("errorDescr").value]
+      end
     end
     
 

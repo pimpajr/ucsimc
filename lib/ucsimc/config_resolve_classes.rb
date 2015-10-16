7dd03f36269ad1dc584577810890eb44432526ae
patrickjr <patrickjr@yosemite.edu>
2015-10-16 10:49:09 -0700

    added support for resolving classes

---
+++ lib/ucsimc/config_resolve_classes.rb
@@ -0,0 +1,28 @@
+require 'ucsimc/trivial_xml'
+
+module Ucsimc
+  class ConfigResolveClasses < TrivialXml
+    attr_accessor :action, :action_property, :inner_content, :req, :classes
+    attr_reader :cookie
+    
+    def initialize
+      xml_opts = {
+        :inner => "inIds",
+        :inner_id => "Id",
+      }
+      super xml_opts
+    end
+    
+    def request cookie, classes
+      @action = "configResolveClasses"
+      @action_properties = {:cookie => cookie, :inHierarchical => @inHierarchical }
+      @inner_content = classes
+      easy_xml
+    end
+    
+    def response resp
+      easy_response resp
+    end
+    
+  end
+end

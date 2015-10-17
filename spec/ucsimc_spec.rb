fe8a09a649c18797d066781ca0b29f987c391c6f
Patrick Pimentel Jr <existsec@gmail.com>
2015-10-17 11:48:37 -0700

    changed raise_error in spec test

--- spec/ucsimc_spec.rb
+++ spec/ucsimc_spec.rb
@@ -5,7 +5,7 @@ describe Ucsimc do
   it 'has IMC.connect and processes properly' do
    expect {
      Ucsimc::IMC.connect :host=>"127.0.0.1", :user=>"admin", :pass=>"pass", :verify_ssl=>"false"
-   }.to raise_error(RestClient::BadRequest)
+   }.to raise_error
   end
   
   it 'has a version number' do

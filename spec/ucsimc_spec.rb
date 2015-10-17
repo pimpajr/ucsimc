96492629ac19f76515aa547621c9dacb4a6d4bac
Patrick Pimentel Jr <existsec@gmail.com>
2015-10-17 12:28:52 -0700

    added additional spec tests for build

--- spec/ucsimc_spec.rb
+++ spec/ucsimc_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 require_relative '../lib/ucsimc'
+require_relative '../lib/ucsimc/aaa'
+
 describe Ucsimc do
 
   it 'has IMC.connect and processes properly' do
@@ -10,6 +12,16 @@ describe Ucsimc do
   
   it 'has a version number' do
     expect(Ucsimc::VERSION).not_to be nil
+  end
+  
+  it 'has class aaa and method aaalogin builds xml payloads correctly' do
+    aaa = Ucsimc::Aaa.new
+    expect(aaa.aaalogin "user", "pass").to eq("<aaaLogin inName=\"user\" inPassword=\"pass\"/>")
+  end
+  
+  it 'has class ConfigResolveClass and method request that builds xml correctly' do
+    crc = Ucsimc::ConfigResolveClass.new "fake-cookie"
+    expect(crc.request "fabricVlan").to eq("<configResolveClass cookie=\"fake-cookie\" classId=\"fabricVlan\" inHierarchical=\"false\">\n  <inFilter/>\n</configResolveClass>")
   end
 
 end

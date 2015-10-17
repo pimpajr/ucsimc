78a3cd9afdec7694c07e48841cece94d2c246031
Patrick Pimentel Jr. <existsec@gmail.com>
2015-10-17 11:13:54 -0700

    testing travis build

--- spec/ucsimc_spec.rb
+++ spec/ucsimc_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
-
+require_relative '../lib/ucsimc'
 describe Ucsimc do
+
+  it 'has IMC.connect and processes properly' do
+   expect {
+     Ucsimc::IMC.connect :host=>"127.0.0.1", :user=>"admin", :pass=>"pass", :verify_ssl=>"false"
+   }.to raise_error(RestClient::BadRequest)
+  end
+  
   it 'has a version number' do
     expect(Ucsimc::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
